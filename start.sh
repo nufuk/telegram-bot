eca18833c61c9667ae11558e8491420863aeb020
Ufuk <info@ufuk.com>
2024-11-25 20:29:24 +0100

    changes

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 source hisse/bin/activate
-python3 notifyTelegram.py
+python3 notifyTelegram.py &>/dev/null &
+
+process_id=`/bin/ps -fu $USER| grep "notifyTelegram.py" | grep -v "grep" | awk '{print $2}'`
+
+echo $process_id > pid
